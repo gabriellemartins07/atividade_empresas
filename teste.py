a52149cb66073c0571613c99bf0b4bbd90029638
Gabrielle Martins <gabrielle.martins@academico.ifpb.edu.br>
2026-06-08 10:35:49 -0300

    Implement partner companies section in Streamlit app
    Added a new layout for partner companies with images and descriptions.

--- teste.py
+++ teste.py
@@ -1,5 +1,42 @@
 import streamlit as st
 
-st.image("teste.png")
-st.write("Site Dinaldo Jorge")
-st.link_button("Acessar", "https://www.dinaldo.com.br")
+st.set_page_config(page_title="Empresas Parceiras", layout="wide")
+
+st.title("🌐 Empresas Parceiras")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.image("spacex.png")
+    st.subheader("🚀 SpaceX")
+
+    st.write("""
+    Empresa aeroespacial criada por Elon Musk.
+    Atua no desenvolvimento de foguetes e viagens espaciais.
+    """)
+
+    st.link_button("Acessar Site", "https://www.spacex.com/")
+
+with col2:
+    st.image("apple.png")
+    st.subheader("🍎 Apple")
+
+    st.write("""
+    Empresa mundialmente conhecida pelos seus produtos tecnológicos,
+    como iPhone, iPad e MacBook.
+    """)
+
+    st.link_button("Acessar Site", "https://www.apple.com/br/")
+  
+with col3:
+    st.image("netflix.png")
+    st.subheader("🎬 Netflix")
+
+    st.write("""
+    Plataforma de streaming com filmes, séries e documentários
+    assistidos no mundo inteiro.
+    """)
+
+    st.link_button("Acessar Site", "https://www.netflix.com/br/")
+
+--
